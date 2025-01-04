bc692dc872b7fed6c5a13b34497ad7e58db08a41
gauravdubey19 <dubeygaurav520@gmail.com>
2025-01-04 12:04:02 +0530

    auth Provider context

--- context/AuthProvider.jsx
+++ context/AuthProvider.jsx
@@ -1,9 +1,22 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 const AuthProvider = ({ children, session }) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  const [updSession, setUpdSession] = useState(session);
+  useEffect(() => {
+    console.log("Session:", session);
+
+    setUpdSession(session);
+    if (session?.user?._id) {
+      () => {
+        setUpdSession(session);
+      };
+    }
+  }, [session]);
+
+  return <SessionProvider session={updSession}>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
